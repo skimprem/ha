e300ab55d59ce15fd07af5fca476577d44cc9211
Roman Sermyagin <roman@geod.ru>
2018-09-12 17:55:15 +0300

    modified:   ha.f08 	modified:   run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 bin=./build/ha.exe
-data=./data/GAOP2018_2m.nc
-og=test.tst
+#data=./data/GAOP2018_2m.nc
+#data=./data/gao2012.nc
+data=./data/DTU10GRA_2min.nc
+og=grid.tst
 vm="[verbose_mode]"
 hm=dh
-oc=$data"_coef"
+oc=coef.tst
 
 $bin\
   -nf $data\
   -hm $hm\
+  -nm stdout\
   -oc $oc\
-  #-og $og\
-  #-nm stdout\
+  -og $og\
   #--verbose $vm
